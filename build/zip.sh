b54412acd44d3d8270d0a0df9a33c7b589e1cd73
Simon Asika <asika32764@gmail.com>
2014-03-27 17:25:51 +0800

    Update gh-pages

--- build/zip.sh
+++ build/zip.sh
@@ -2,6 +2,6 @@
 
 ROOT=$(cd `dirname $0`; pwd)/..
 
-rm -f $ROOT/assets/bootstrap3-adapter.zip
+rm -f $ROOT/docs/assets/bootstrap3-adapter.zip
 
-zip $ROOT/assets/bootstrap3-adapter.zip $ROOT/dist/css/bootstrap3-adapter.css $ROOT/dist/css/bootstrap3-adapter.min.css
+zip $ROOT/docs/assets/bootstrap3-adapter.zip $ROOT/dist/css/bootstrap3-adapter.css $ROOT/dist/css/bootstrap3-adapter.min.css
